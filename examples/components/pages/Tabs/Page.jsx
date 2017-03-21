e6355d78461c983fbfd1eb6a2d3280240ef506c2
Shawn McKnight <smcknight@storis.com>
2017-03-21 19:10:51 -0400

    Limit size of examples to ensure scrolling behavior present regardless of viewport

--- examples/components/pages/Tabs/Page.jsx
+++ examples/components/pages/Tabs/Page.jsx
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
+import withWidth, {MEDIUM, LARGE} from 'material-ui/utils/withWidth';
 
 import CodeExample from '../../CodeExample';
 
@@ -10,19 +11,48 @@ const descriptions = {
 };
 
 class TabPage extends Component {
+  static propTypes = {
+    width: PropTypes.number.isRequired,
+  }
+
+  getStyles = () => {
+    const styles = {
+      sizeLimiter: {
+        width: '100%',
+        margin: '0 auto',
+        border: '1px solid black',
+        padding: '1rem',
+        boxSizing: 'border-box',
+      },
+      sizeLimiterMedium: {
+        width: '671px',
+      },
+    };
+
+    if (this.props.width === MEDIUM || this.props.width === LARGE) {
+      styles.sizeLimiter = {...styles.sizeLimiter, ...styles.sizeLimiterMedium};
+    }
+
+    return styles;
+  }
+
   render() {
+    const styles = this.getStyles();
+
     return (
-      <div>
+      <div style={{margin: '24px'}}>
         <CodeExample
           title="Simple Scrolling"
           description={descriptions.simple}
           code={scrollingSimpleCode}
         >
-          <ScrollingSimple />
+          <div style={styles.sizeLimiter}>
+            <ScrollingSimple />
+          </div>
         </CodeExample>
       </div>
     );
   }
 }
 
-export default TabPage;
+export default withWidth()(TabPage);
